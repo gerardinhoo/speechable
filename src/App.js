dfe000f121aa857ebfc353fab0fa672f372bf1bf
Gerard Eklu <gerard_eklu@homedepot.com>
2020-06-21 20:20:14 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
+import UserInfo from "./components/UserInfo/UserInfo";
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <UserInfo />
       <Sidebar />
     </div>
   );
---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./UserInfo.css"
+
+const UserInfo = () => {
+  return (
+    <div className="img-content">
+       <div>
+        <img src="https://platform.nashvilleparent.com/media/cellkid.jpg" width="10%" alt="" />
+        <div class='photo-name'>Name</div>
+       </div>
+      <div>
+       <img src="https://platform.nashvilleparent.com/media/cellkid.jpg" width="10%" alt="" />
+       <div class='photo-name'>Name</div>
+      </div>
+    </div>
+ 
+  )
+}
+
+export default UserInfo;
